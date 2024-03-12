e8abfe4b25f54d1358b25d4aa074758d769b4fc8
skyincinnamon <161468031+skyincinnamon@users.noreply.github.com>
2024-03-12 21:25:53 +0800

    第五次作业
    JavaScript啦啦啦

---
+++ script.js
@@ -0,0 +1,10 @@
+document.write("hello world");
+function sayhello(){
+  var response=prompt("what's your name?");
+  alert("hello "+response+"u so beautiful");
+};
+function showtopic(){
+  var x=document.getElementById('demo');
+  x.style.fontSize="25px";
+  x.style.color="red";
+};
